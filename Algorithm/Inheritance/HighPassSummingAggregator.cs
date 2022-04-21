65aa405f3a94705903acd0aadb23f76af6c45934
Suyash <suyash.kathuria7@gmail.com>
2022-04-21 16:18:04 +1200

    Implemented the HighPassSummingAggregator class inheriting from the SummingAggregator class

--- Algorithm/Inheritance/HighPassSummingAggregator.cs
+++ Algorithm/Inheritance/HighPassSummingAggregator.cs
@@ -7,8 +7,16 @@ namespace Algorithm.Inheritance
     /// Should filter out measurements with an X or Y that are less than or equal to 2
     /// You'll need to inherit and override methods from other classes in the inheritance folder
     /// </summary>
-    public class HighPassSummingAggregator 
+    public class HighPassSummingAggregator : SummingAggregator
     {
+        public HighPassSummingAggregator(IEnumerable<Measurement> measurements) : base(measurements)
+        {
+            
+        }
 
-    }    
+        protected override IEnumerable<Measurement> FilterMeasurements(IEnumerable<Measurement> measurements)
+        {
+            return measurements.Where(m => m.X >= 5 && m.Y >= 5);
+        }
+    }
 }
